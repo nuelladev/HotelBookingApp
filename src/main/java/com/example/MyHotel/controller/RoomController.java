e9845f0108045bd0601189f5ed3ab8f027aec35b
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 10:25:31 +0100

    features: added controller

--- src/main/java/com/example/MyHotel/controller/RoomController.java
+++ src/main/java/com/example/MyHotel/controller/RoomController.java
@@ -1,4 +1,14 @@
 package com.example.MyHotel.controller;
 
+import com.example.MyHotel.response.RoomResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
 public class RoomController {
+    public ResponseEntity<RoomResponse>
+    addNewRoom(MultipartFile roomPhoto, String roomType, BigDecimal roomPrice){
+
+    }
 }
