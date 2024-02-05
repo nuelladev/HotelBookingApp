2d091d7a64466f1746f71215393b53e3322aea02
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 09:01:09 +0100

    features: added DTO

--- src/main/java/com/example/MyHotel/response/RoomResponse.java
+++ src/main/java/com/example/MyHotel/response/RoomResponse.java
@@ -1,4 +1,16 @@
 package com.example.MyHotel.response;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Data
+@NoArgsConstructor
 public class RoomResponse {
-}
+    private Long id;
+    private String roomType;
+    private BigDecimal roomPrice;
+    private boolean isBooked;
+    private String photo;
+    private List<BookingResponse>bookings;
