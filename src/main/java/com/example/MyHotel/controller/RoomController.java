e17af4f371f9dfde8dcfac6a5ff2ca18600e1a88
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 11:45:48 +0100

    Bugfix: fixed some bugs

--- src/main/java/com/example/MyHotel/controller/RoomController.java
+++ src/main/java/com/example/MyHotel/controller/RoomController.java
@@ -5,20 +5,27 @@ import com.example.MyHotel.response.RoomResponse;
 import com.example.MyHotel.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.SQLException;
+
 @RestController
+@RequestMapping("/rooms")
 @RequiredArgsConstructor
 public class RoomController {
 
     private final RoomService roomService;
-    public ResponseEntity<RoomResponse>
-    addNewRoom(@RequestParam("roomPhoto") MultipartFile roomPhoto,
+    @PostMapping("add/new-room")
+    public ResponseEntity<RoomResponse> addNewRoom
+            (@RequestParam("roomPhoto") MultipartFile roomPhoto,
                @RequestParam("roomType")String roomType,
-               @RequestParam("roomPrice")BigDecimal roomPrice){
+               @RequestParam("roomPrice")BigDecimal roomPrice) throws SQLException, IOException {
         Room savedRoom = roomService.addNewRoom(roomPhoto, roomType, roomPrice);
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
