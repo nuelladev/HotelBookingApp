fd1280e3f34ae8a311c725a72ece9c246312e067
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 09:26:34 +0100

    features: initialized constructors

--- src/main/java/com/example/MyHotel/response/RoomResponse.java
+++ src/main/java/com/example/MyHotel/response/RoomResponse.java
@@ -13,5 +13,21 @@ public class RoomResponse {
     private String roomType;
     private BigDecimal roomPrice;
     private boolean isBooked;
-    private String photo;
+    private String roomPhoto;
     private List<BookingResponse> bookings;
+
+    public RoomResponse(Long id, BigDecimal roomPrice, boolean isBooked) {
+        this.id = id;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+    }
+
+    public RoomResponse(Long id, String roomType, BigDecimal roomPrice, boolean isBooked, String roomPhoto, List<BookingResponse> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.isBooked = isBooked;
+        this.roomPhoto = roomPhoto;
+        this.bookings = bookings;
+    }
+}
