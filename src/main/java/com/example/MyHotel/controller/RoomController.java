4f2bd2bec310f71f8f55a9c17780cc7b442d9546
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 10:34:41 +0100

    features: added service layer

--- src/main/java/com/example/MyHotel/controller/RoomController.java
+++ src/main/java/com/example/MyHotel/controller/RoomController.java
@@ -1,17 +1,23 @@
 package com.example.MyHotel.controller;
 
+import com.example.MyHotel.model.Room;
 import com.example.MyHotel.response.RoomResponse;
+import com.example.MyHotel.service.RoomService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.math.BigDecimal;
-
+@RequiredArgsConstructor
 public class RoomController {
+
+    private final RoomService roomService;
     public ResponseEntity<RoomResponse>
     addNewRoom(@RequestParam("roomPhoto") MultipartFile roomPhoto,
                @RequestParam("roomType")String roomType,
                @RequestParam("roomPrice")BigDecimal roomPrice){
+        Room savedRoom = roomService.addNewRoom(roomPhoto, roomType, roomPrice)
 
     }
 }
--- src/main/java/com/example/MyHotel/service/RoomService.java
+++ src/main/java/com/example/MyHotel/service/RoomService.java
@@ -1,4 +1,10 @@
 package com.example.MyHotel.service;
 
+import com.example.MyHotel.model.Room;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.math.BigDecimal;
+
 public interface RoomService {
+    Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice);
 }
