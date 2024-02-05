6d5fe2f3673f30472a3bb69d734cae3d6f0484d6
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 10:58:45 +0100

    features: started implementations for add service layer

--- src/main/java/com/example/MyHotel/service/RoomServiceImpl.java
+++ src/main/java/com/example/MyHotel/service/RoomServiceImpl.java
@@ -1,13 +1,27 @@
 package com.example.MyHotel.service;
 
 import com.example.MyHotel.model.Room;
+import com.example.MyHotel.repository.RoomRepository;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.sql.rowset.serial.SerialBlob;
+import java.io.IOException;
 import java.math.BigDecimal;
+import java.sql.Blob;
+import java.sql.SQLException;
 
 public class RoomServiceImpl implements RoomService {
+    private RoomRepository roomRepository;
     @Override
-    public Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) {
-        return null;
+    public Room addNewRoom(MultipartFile file, String roomType, BigDecimal roomPrice throws SQLException, IOException) {
+        Room room = new Room();
+        room.setRoomType(roomType);
+        room.setRoomPrice(roomPrice);
+        if(!file.isEmpty()){
+            byte[] photoBytes = file.getBytes();
+            Blob photoBlob = new SerialBlob(photoBytes);
+            room.setRoomPhoto(photoBlob);
+        }
+        return room
     }
 }
