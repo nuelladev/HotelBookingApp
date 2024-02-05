d15c386582d72d1599c32adb3f7b8d324bec2b48
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 10:49:35 +0100

    features: started implementations for add service layer

--- src/main/java/com/example/MyHotel/service/RoomServiceImpl.java
+++ src/main/java/com/example/MyHotel/service/RoomServiceImpl.java
@@ -1,5 +1,13 @@
 package com.example.MyHotel.service;
 
-public class RoomServiceImpl implements RoomService {
+import com.example.MyHotel.model.Room;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.math.BigDecimal;
+
+public class RoomServiceImpl implements RoomService {
+    @Override
+    public Room addNewRoom(MultipartFile photo, String roomType, BigDecimal roomPrice) {
+        return null;
+    }
 }
