95896dad8c4bf7ccc35a47b28f11f1a07d77d63d
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-05 09:35:33 +0100

    features: added DTO for booking response

--- src/main/java/com/example/MyHotel/response/BookingResponse.java
+++ src/main/java/com/example/MyHotel/response/BookingResponse.java
@@ -1,4 +1,28 @@
 package com.example.MyHotel.response;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookingResponse {
+
+    private Long id;
+
+    private LocalDate checkInDate;
+
+    private LocalDate checkOutDate;
+
+    private String guestName;
+
+    private String guestEmail;
+
+    private int numOfAdults;
+
+    private int numOfChildren;
+
+    private int totalNumOfGuests;
+
+    private String bookingConfirmationCode;
+
+    private RoomResponse room;
+
 }
