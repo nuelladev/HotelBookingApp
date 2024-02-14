b613e59db3fa95727845abb792bb887a8b0fd566
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-14 12:43:25 +0100

    features: added new features

--- src/main/java/com/example/MyHotel/controller/RoomController.java
+++ src/main/java/com/example/MyHotel/controller/RoomController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.SQLException;
+import java.util.List;
 
 @RestController
 @RequestMapping("/rooms")
@@ -31,5 +32,7 @@ public class RoomController {
         return ResponseEntity.ok(response);
     }
 
+  public List<String>getRoomTypes(){
 
+  }
 }
