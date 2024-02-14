eb18b7bde927f9de24fc5e5cd6c7e1d7c8b9c634
nuelladev <emmanuellaokafor42@yahoo.com>
2024-02-14 12:48:26 +0100

    features: added new features

--- src/main/java/com/example/MyHotel/controller/RoomController.java
+++ src/main/java/com/example/MyHotel/controller/RoomController.java
@@ -5,10 +5,7 @@ import com.example.MyHotel.response.RoomResponse;
 import com.example.MyHotel.service.RoomService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
@@ -31,8 +28,8 @@ public class RoomController {
         RoomResponse response = new RoomResponse(savedRoom.getId(), savedRoom.getRoomType(), savedRoom.getRoomPrice());
         return ResponseEntity.ok(response);
     }
-
+@GetMapping("/room/types")
   public List<String>getRoomTypes(){
-
+   return roomService.getAllRoomTypes();
   }
 }
